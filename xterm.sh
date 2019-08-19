014e39aae8ef93c21995143c6337df8de0f2e5a3
Arjan van Olphen <arjan@scanframe.nl>
2019-08-19 23:23:48 +0200

    Added terminal script.

--- xterm.sh
+++ xterm.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
 #cygstart 
-setsid xterm -T "Xterm" -geometry 140x45 +ai -bg black -fg white -sl 3000 -fs 10 -fa 'Lucida Console'
+setsid xterm -T "Xterm" -geometry 140x45 +ai -bg black -fg white -sl 3000 +bdc -fs 10 -fa 'Noto Mono' 
