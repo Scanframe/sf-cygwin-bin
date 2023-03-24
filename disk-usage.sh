35d37da1ebf2604d03d71ab28a5c51fc244d086f
U-WERK\aolphen <aolphen@VW46637.werk.local>
2023-03-24 22:21:50 +0100

    Added some usefull scripts.

---
+++ disk-usage.sh
@@ -0,0 +1,35 @@
+#!/bin/bash 
+
+#set -x 
+
+if [ -z "$1" ]; then
+
+	echo "Usage: $0 <depth> [directory]"
+	echo "Reports size of each directory on the same file system."
+	exit 1
+	
+fi
+
+OPTIONS="--one-file-system --dereference-args --si --max-depth ${1}"
+
+
+if [ $1 != 0 ] ; then
+
+OPTIONS="--total ${OPTIONS}"
+
+fi
+
+
+if [ -z "$2" ]; then
+
+	DIR="$(pwd)"
+
+else
+
+	DIR="${2}"
+
+fi
+
+
+exec du ${OPTIONS} "${DIR}" | sort -h
+
