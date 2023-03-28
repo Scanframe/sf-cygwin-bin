cc03cd6de79a3d5666731c50cf521b409ec83a57
Arjan van Olphen <arjan@scanframe.nl>
2023-03-28 22:41:01 +0200

    Fix

--- xserver-start.sh
+++ xserver-start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# Get the bash script directory.
+SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 # If mintty is running do not start the X-server.
 if [[ $(pgrep mintty | wc -l) -eq 1 ]]; then
 	# Start the X-server in the background.
-	"${DIR}/vcxsrv.bat" 2>&1 /dev/null &
+	"${SCRIPT_DIR}/vcxsrv.bat" 2>&1 /dev/null &
 fi
