bcac1aea2c059ad2b8c8c554d5567d5643e2d871
Arjan van Olphen <arjan@scanframe.nl>
2023-04-04 13:22:49 +0200

    Added VT enable option to vm script.

--- vm-git-start.sh
+++ vm-git-start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+VM_ID='{4135ba50-f8fc-4c87-a498-9f95d85eddb7}'
+
 function ShowHelp()
 {
 	cat << HEREDOC
@@ -7,11 +9,10 @@ Usage :$0 <start|stop|off>
   start : Starts the VM headless.
   stop  : Stops the VM using an ACPI signal (power button).
   off   : Kills the VM.
-	ip    : Returns the VM assigned IP.
+  ip    : Returns the VM assigned IP.
+  vt    : Enables nested VT-x/AMD-V on VM.
 HEREDOC
 }
-
-VM_ID='{4135ba50-f8fc-4c87-a498-9f95d85eddb7}'
 
 case "$1" in
 
@@ -32,6 +33,10 @@ case "$1" in
 
 	ip)
 		VBoxManage guestproperty get "${VM_ID}" "/VirtualBox/GuestInfo/Net/0/V4/IP" | cut -d' ' -f2
+		;;
+
+	vt)
+		VBoxManage modifyvm "${VM_ID}" --nested-hw-virt on
 		;;
 
 	*)
