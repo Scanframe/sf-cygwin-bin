a20b044a5bf56de3af8a4f0a459a4f32b4445504
Arjan van Olphen <arjan@scanframe.nl>
2023-04-08 00:01:07 +0200

    Fixed X-server cursor size when using VM.

--- vcxsrv.bat
+++ vcxsrv.bat
@@ -1,1 +1,5 @@
-@"%ProgramFiles%\VcXsrv\vcxsrv.exe" :0 -dpi auto -ac -lesspointer -multiwindow -multimonitors -hostintitle -clipboard -wgl -nounixkill -nowinkill -silent-dup-error -fp fonts/dejavu/,fonts/misc/,fonts/TTF/,fonts/OTF,fonts/Type1/,fonts/100dpi/,fonts/75dpi/,fonts/cyrillic/,fonts/Speedo/,built-ins
+@echo off
+
+"%ProgramFiles%\VcXsrv\vcxsrv.exe" :0 -dpi auto -ac -lesspointer -multiwindow -multimonitors -hostintitle -clipboard -noprimary +bs -wgl -swrastwgl -nounixkill -nowinkill -silent-dup-error -fp fonts/dejavu/,fonts/misc/,fonts/TTF/,fonts/OTF,fonts/Type1/,fonts/100dpi/,fonts/75dpi/,fonts/cyrillic/,fonts/Speedo/,built-ins
+
+:: -noclipboardprimary
