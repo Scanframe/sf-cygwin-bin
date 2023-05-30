53aeebf3f74714df061deb6a73176dc5c7817e64
Arjan van Olphen <arjan@scanframe.nl>
2023-05-30 11:10:06 +0200

    Check on different process for starting X-server.

--- xserver-start.sh
+++ xserver-start.sh
@@ -2,8 +2,8 @@
 
 # Get the bash script directory.
 SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
-# If mintty is running do not start the X-server.
-if [[ $(pgrep mintty | wc -l) -eq 1 ]]; then
+# If the batch file is running do not start the X-server.
+if [[ $(pgrep vcxsrv.bat | wc -l) -eq 0 ]]; then
 	# Start the X-server in the background.
 	"${SCRIPT_DIR}/vcxsrv.bat" 2>&1 /dev/null &
 fi
