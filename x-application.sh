523012f744b7b73612633954e1da45471c94a1cc
Arjan van Olphen <arjan@scanframe.nl>
2023-08-02 09:11:23 +0200

    Simplified...

--- x-application.sh
+++ x-application.sh
@@ -9,4 +9,4 @@ fi
 IP_HOST_ONLY="$(ipconfig /all | grep "VirtualBox Host-Only Ethernet Adapter" -A 4 | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}')"
 
 # Run the ssh command passing the DISPLAY environment variable.
-ssh super@kubuntu -t "DISPLAY=${IP_HOST_ONLY}${DISPLAY} XCURSOR_SIZE=16 KDE_FULL_SESSION=true /bin/bash -c 'source ~/.profile && $1'"
+ssh super@kubuntu -t "DISPLAY=${IP_HOST_ONLY}${DISPLAY} XCURSOR_SIZE=16 KDE_FULL_SESSION=true /bin/bash --login -c '$1'"
