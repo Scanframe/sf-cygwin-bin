fd32bf9bca38887f299ece81c786cb1b6aa98c7b
Arjan van Olphen <arjan@scanframe.nl>
2025-01-24 17:20:30 +0100

    Fix for when X11 server is not present when asking a password.

--- ssh-askpasswd.sh
+++ ssh-askpasswd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [[ -z "${DISPLAY}" ]]; then
+if [[ -z "${DISPLAY}" ]] || ! command -v zenity >/dev/null; then
 	dialog --passwordbox "SSH Password" 10 70
 else
-	zenity --title="Enter SSH Key Password " --password
+	zenity --title="Enter SSH Key Password " --window-icon="dialog-password" --password
 fi
