0bec3a7403d27bbc2e2066789a1cff94c5fb82ff
Arjan van Olphen <arjan@scanframe.nl>
2025-01-26 14:07:59 +0100

    Added symlink conversion script.

---
+++ symlink2native.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Bailout on first error.
+set -e
+
+# Show usage help when no argument is passed on the command line.
+if [[ -z "$1" ]]; then
+	echo "Convert a Cygwin JUNCTION symlinks into Windows native SYMLINK or SYMLINKD symlink types.
+Usage: $0 <directory>
+"
+	exit 1
+fi
+
+target_dir="$1"
+magic="-#symlink-rename#"
+
+# Check if the directory exists.
+if [[ ! -d "${target_dir}" ]]; then 
+	echo "Target directory '${target_dir}' does not exist!"
+	exit 1
+fi 
+
+pushd "${target_dir}" >/dev/null
+while read -r symlink; do
+	if fsutil reparsepoint query "$(cygpath -w "$(dirname "${symlink}")")\\$(basename "${symlink}")" | 
+		head -n 4 | grep 'Tag value: Symbolic Link' >/dev/null ; then
+		continue
+	fi
+	echo "Converting: ${symlink}"
+	dest="$(readlink "${symlink}")"
+	echo "Destination: ${dest}"
+	CYGWIN=winsymlinks:native ln -rs "${dest}" "${symlink}${magic}"
+	# Finally replace the original symlink.
+	mv "${symlink}${magic}" "${symlink}"
+done < <(find ./ -maxdepth 1 -type l)
+popd >/dev/null
+
